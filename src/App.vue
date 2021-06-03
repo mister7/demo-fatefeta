7bea32c9219d833bc89253de51e7a1f5568b1329
mister7 <mister7@jzcha.com>
2021-06-03 09:13:03 +0800

    chore(HelloWorld, Login): rename
    Find the way to rename a vue file:
    
    1. Rename the src/components/HelloWorld.vue to src/components/Login.vue;
    2. Modify the line 96 in the file renamed;
    3. Modify the lines 41, 47, 53 in file App.vue.

--- src/App.vue
+++ src/App.vue
@@ -38,19 +38,19 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <Login/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Login from './components/Login';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Login,
   },
 
   data: () => ({
